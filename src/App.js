19528211d6161048eb4a2ceebbd776f9d5971957
marcoamd <marcoamd.com@gmail.com>
2019-04-12 20:52:54 -0500

    Avance 12-04-2019

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import WeatherLocation from './components/WeatherLocation';
 import LocationList from './components/WeatherLocation/LocationList';
 import { any } from 'prop-types';
+import { Container, Row, Col, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -14,9 +15,30 @@ class App extends Component {
       "Ica, PE",
     ];
     return (
-      <div className="App">
-        <LocationList cities={cities}/>
-      </div>
+      <Container>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar>
+        <br />
+        <Row>
+          <h1>Titutlo</h1>
+        </Row>
+        <Row>
+          <h5>API de climas y pronóstico</h5>
+        </Row>
+        <Row>
+          <LocationList cities={cities} />
+        </Row>
+      </Container>
     );
   }
 }
--- src/components/WeatherLocation/LocationList.js
+++ src/components/WeatherLocation/LocationList.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import WeatherLocation from './index';
 
-const LocationList = (props)=>{
-
+const LocationList = (props) => {
+    //     let cities = props.cities;
+    //     console.log(cities[0]);
+    //     return (<div>
+    //         {createWeatherLocations(cities)}
+    //     </div>);
     let cities = props.cities;
-    console.log(cities[0]);
     return (<div>
-        {createWeatherLocations(cities)}
+        {generateWeatherData(cities)}
     </div>);
 }
-
-const createWeatherLocations = (cities)=>{
-    let weatherLocations = [];
-    for(let city of cities)
-    {
-        weatherLocations.push(<WeatherLocation city={city}/>);
-    }
+const generateWeatherData = (cities) => {
+    let weatherLocations = cities.map((city, i) => {
+        return <WeatherLocation city={city} key={i} />
+    });
     return weatherLocations;
+}
+
+const createWeatherLocations = (cities) => {
+    // let weatherLocations = [];
+    // for(let city of cities)
+    // {
+    //     weatherLocations.push(<WeatherLocation city={city}/>);
+    // }
+    // return weatherLocations;
+
 }
 
 export default LocationList;
